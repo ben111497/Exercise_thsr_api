745ef0cd8a3e9735cd398887c473842f515be83e
yu shu lin <ben111497@gmail.com>
2021-01-02 17:24:57 +0800

    adjust search layout

--- app/src/main/java/com/example/lab12/MainActivity2.kt
+++ app/src/main/java/com/example/lab12/MainActivity2.kt
@@ -11,44 +11,51 @@ import android.view.KeyEvent
 import android.view.View
 import android.widget.*
 import androidx.core.view.get
+import com.example.lab12.adapter.StationListAdapter
+import com.example.lab12.adapter.StationSearchAdapter
+import com.example.lab12.manager.DialogManager
 import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main_homepage.*
 import androidx.core.widget.addTextChangedListener as addTextChangedListener1
 
 class MainActivity2 : AppCompatActivity() {
-    private lateinit var dbrw : SQLiteDatabase
-    private var items : ArrayList<String> = ArrayList(0)
+    class Station(val name: String, val address: String, val positionLat: String, val positionLon: String)
 
-    private lateinit var adapter : ArrayAdapter<String>
+    private lateinit var dbrw : SQLiteDatabase
+    private var items = ArrayList<Station>()
+    private var originData = ArrayList<Station>()
+
+    private lateinit var adapter : StationSearchAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         dbrw=MyDBHelper(this).writableDatabase
         //取得資料庫實體
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1 , items)
+
+        adapter = StationSearchAdapter(this, items, object: StationListAdapter.MsgListener{
+            override fun onClick(position: Int) {
+                val bundle = Bundle()
+                bundle.putString("PositionLat", items[position].positionLat)
+                bundle.putString("PositionLon", items[position].positionLon)
+                val intent = Intent().putExtras(bundle)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        })
+
         listview.adapter = adapter
 
         //宣告 Adapter，使用 simple_list_item_1並連結 listView
         val c = dbrw.rawQuery( "SELECT * FROM myTable",null)
-        var PositionLat = arrayOfNulls<String>(c.count)
-        var PositionLon = arrayOfNulls<String>(c.count)
         c.moveToFirst()
         items.clear()
         for(i in 0 until c.count){
-            items.add("${c.getString(0)}高鐵站\n ${c.getString(3)}")
-            PositionLat[i]=c.getString(1)
-            PositionLon[i]=c.getString(2)
+            originData.add(Station(c.getString(0),  c.getString(3), c.getString(1), c.getString(2)))
             c.moveToNext()
         }
-        adapter.notifyDataSetChanged()
-        listview.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
-            val bundle = Bundle()
-            bundle.putString("PositionLat", PositionLat[position].toString())
-            bundle.putString("PositionLon", PositionLon[position].toString())
-            val intent = Intent().putExtras(bundle)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        })
+        items.addAll(originData)
         c.close()
+        
         name.addTextChangedListener(object:TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
 
@@ -57,29 +64,11 @@ class MainActivity2 : AppCompatActivity() {
 
             }
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                val c = dbrw.rawQuery( if(name.length()<1 ){ "SELECT * FROM myTable"}
-                else {"SELECT * FROM myTable WHERE StationName LIKE '%${name.text}%' OR StationAddress LIKE '%${name.text}%'"},null)
-                c.moveToFirst()
                 items.clear()
-                for(i in 0 until c.count){
-                    items.add("${c.getString(0)}高鐵站\n${c.getString(3)}")
-                    PositionLat[i]=c.getString(1)
-                    PositionLon[i]=c.getString(2)
-                    c.moveToNext()
-                }
+                items.addAll(originData.filter { it.name.contains(p0.toString()) || it.address.contains(p0.toString()) })
                 adapter.notifyDataSetChanged()
-                listview.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
-                    val bundle1 = Bundle()
-                    bundle1.putString("PositionLat", PositionLat[position].toString())
-                    bundle1.putString("PositionLon", PositionLon[position].toString())
-                    val intent = Intent().putExtras(bundle1)
-                    setResult(Activity.RESULT_OK, intent)
-                    finish()
-                })
-                c.close()
             }
         })
-
     }
 }
 
